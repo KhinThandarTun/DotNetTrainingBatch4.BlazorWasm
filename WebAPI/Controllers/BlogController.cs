a38681940eb0a481cbd92688753386f04e41ff54
Khin Thandar Tun <106916866+KhinThandarTun@users.noreply.github.com>
2024-09-08 17:03:28 +0630

    Add project files.

---
+++ WebAPI/Controllers/BlogController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public BlogController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetBlogs() 
+        {
+            var list = _context.Blogs.ToList();
+            return Ok(list);
+        }
+
+        [HttpPost]
+        public IActionResult CreateBlog(Blog model)
+        {
+            _context.Blogs.Add(model);
+            var result = _context.SaveChanges();
+
+            string message = result > 0 ? "Saving Successful!" : "Saving Failed!";
+            return Ok(message);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateBlog()
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+
+        public IActionResult DeleteBlog() 
+        {
+            return Ok();
+        }
+    }
+}
---
+++ WebAPI/Models/AppDbContext.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebAPI.Models;
+
+public partial class AppDbContext : DbContext
+{
+    public AppDbContext()
+    {
+    }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Blog> Blogs { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Server=.;Database=DotNetTrainingBatch4;User ID=sa;Password=123;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Blog>(entity =>
+        {
+            entity.ToTable("Blog");
+
+            entity.Property(e => e.BlogId).ValueGeneratedNever();
+            entity.Property(e => e.BlogAuthor).HasMaxLength(256);
+            entity.Property(e => e.BlogContent).HasMaxLength(256);
+            entity.Property(e => e.BlogTitle).HasMaxLength(256);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
